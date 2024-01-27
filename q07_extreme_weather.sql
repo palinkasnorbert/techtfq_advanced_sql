21b95bd415d58240ff08cb40d08490acc8d6448b
Norbert Palinkas <norbert@palinkasnorbert.hu>
2024-01-27 20:14:01 +0100

    query 7 upload with DDL, DML and solution with comments.

--- q07_extreme_weather.sql
+++ q07_extreme_weather.sql
@@ -6,61 +6,73 @@ Note: Weather is considered to be extremely cold when its temperature is less th
 
 Table Name: WEATHER
 
-Approach: First using a sub query identify all the records where the temperature was very cold and then use a main query to fetch only the records returned as very cold from the sub query. You will not only need to compare the records following the current row but also need to compare the records preceding the current row. And may also need to compare rows preceding and following the current row. Identify a window function which can do this comparison pretty easily.
+Approach (from blog): 
+First using a sub query identify all the records where the temperature was very cold and then use a main query to fetch only the records returned as very cold from the sub query. 
+You will not only need to compare the records following the current row but also need to compare the records preceding the current row. 
+And may also need to compare rows preceding and following the current row. Identify a window function which can do this comparison pretty easily.
+
 */
 
--- Query 7:
+USE techfq;
+GO
 
--- From the weather table, fetch all the records when London had extremely cold temperature for 3 consecutive days or more.
+DROP TABLE IF EXISTS weather;
+GO
 
--- Note: Weather is considered to be extremely cold then its temperature is less than zero.
+BEGIN
+    CREATE TABLE Weather
+    (
+        id INT,
+        city VARCHAR(50),
+        temperature INT,
+        day DATE
+    );
 
---Table Structure:
+    DELETE FROM weather;
 
-drop table if exists weather;
-create table weather
-(
-id int,
-city varchar(50),
-temperature int,
-day date
-);
-delete from weather;
-insert into weather values
-(1, 'London', -1, CAST(N'2021-01-01' AS date)),
-(2, 'London', -2, CAST(N'2021-01-02' AS date)),
-(3, 'London', 4, CAST(N'2021-01-03' AS date)),
-(4, 'London', 1, CAST(N'2021-01-04' AS date)),
-(5, 'London', -2, CAST(N'2021-01-05' AS date)),
-(6, 'London', -5, CAST(N'2021-01-06' AS date)),
-(7, 'London', -7, CAST(N'2021-01-07' AS date)),
-(8, 'London', 5, CAST(N'2021-01-08' AS date));
+    INSERT INTO weather 
+    VALUES
+        (1, 'London', -1, CAST(N'2021-01-01' AS date)),
+        (2, 'London', -2, CAST(N'2021-01-02' AS date)),
+        (3, 'London', 4, CAST(N'2021-01-03' AS date)),
+        (4, 'London', 1, CAST(N'2021-01-04' AS date)),
+        (5, 'London', -2, CAST(N'2021-01-05' AS date)),
+        (6, 'London', -5, CAST(N'2021-01-06' AS date)),
+        (7, 'London', -7, CAST(N'2021-01-07' AS date)),
+        (8, 'London', 5, CAST(N'2021-01-08' AS date));
+END
+GO
 
-select * from weather;
+SELECT * FROM weather;
+
+/* 
+My approach:
+Using CTE for readability, check the prior and following days with LAG and LEAD combinations.
+*/
 
 WITH extreme_cold AS (
     SELECT * FROM weather WHERE temperature < 0 -- filter values for temps below 0.
 ),
 consec_colds AS (
     SELECT *,
-        CASE WHEN -- 2 days after are valid
+        CASE WHEN -- if +1 and +2 days are consecutively coming after current row, then flag as valid with 'Y'.
         DATEADD(day, 1, day) = LEAD(day, 1, NULL) OVER (ORDER BY day) AND
         DATEADD(day, 2, day) = LEAD(day, 2, NULL) OVER (ORDER BY day) 
         THEN 'Y'
-        WHEN -- 1 days before and after are valid
+        WHEN -- if -1 and +1 days are consecutively coming before & after current row, then flag as valid with 'Y'.
         DATEADD(day, -1, day) = LAG(day, 1, NULL) OVER (ORDER BY day) AND
         DATEADD(day, 1, day) = LEAD(day, 1, NULL) OVER (ORDER BY day) 
         THEN 'Y'
-        WHEN -- 2 days before are valid
+        WHEN -- if -1 and -2 days are consecutively coming before current row, then flag as valid with 'Y'.
         DATEADD(day, -1, day) = LAG(day, 1, NULL) OVER (ORDER BY day) AND
         DATEADD(day, -2, day) = LAG(day, 2, NULL) OVER (ORDER BY day) 
         THEN 'Y'
-        ELSE NULL
+        ELSE NULL -- else flag is NULL
         END AS flag
     FROM extreme_cold
 )
 
 SELECT id, city, temperature, day
 FROM consec_colds
-WHERE flag = 'Y'
+WHERE flag = 'Y' -- select flagged, order by day
 ORDER BY day;
